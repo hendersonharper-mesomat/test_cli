63a3496f6290cd1518b26d2d1c223023b690932b
Henderson Harper <henderson.harper@mesomat.com>
2026-09-15 15:21:55 -0400

    feat(example): more examples

--- src/test_cli/example.py
+++ src/test_cli/example.py
@@ -19,3 +19,28 @@ def cli_leave(name: Annotated[str, typer.Argument(help="Name of the person to le
             raise typer.Exit()
         else:
             raise typer.Abort()
+
+@app.command("four-tuple")
+def cli_four_tuple(tup: Annotated[tuple[int, int, int, int], typer.Argument(help="A tuple of four integers.")] = (1,2,3,4)) -> None:
+    """Print the four-tuple."""
+    print(f"The four-tuple is: {tup}")
+
+@app.command("list")
+def cli_list(items: Annotated[list[str], typer.Argument(help="A list of strings.")] = ["item1", "item2", "item3"]) -> None:
+    """Print the list."""
+    print(f"The list is: {items}")
+
+@app.command("range-prompt")
+def cli_range_prompt(
+    start: Annotated[
+        int, typer.Option(
+            prompt=True,
+            help="The starting value of the range."
+        )] = 0, 
+    end: Annotated[
+        int, typer.Option(
+            prompt=True,
+            help="The ending value of the range."
+        )] = 10) -> None:
+    """Print a range of numbers."""
+    print(f"The range is: {list(range(start, end))}")
